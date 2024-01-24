44c854e06c713364d31efce412d45fc70836de2f
mostafahanafi <mostafahanafi2001@hotmail.com>
2024-01-24 15:40:00 +0000

    parse JSON, return in user-friendly way

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -17,9 +18,27 @@ def getroute():
 
     if response.status_code != 200:
         return render_template('error.html')
-    else:
-        data = response.json()
-        return jsonify(data)
+    
+    data = response.json()
+
+    # Extract the time from the date time string
+    for journey in data["journeys"]:
+        journey["startDateTime"] = extract_time(journey["startDateTime"])
+        journey["arrivalDateTime"] = extract_time(journey["arrivalDateTime"])
+        journey["modes"] = []
+        for leg in journey["legs"]:
+            leg["departureTime"] = extract_time(leg["departureTime"])
+            leg["arrivalTime"] = extract_time(leg["arrivalTime"])
+        
+            # Add journey modes
+            journey["modes"].append(leg["mode"]["name"])
+
+    return render_template('route.html', metadata=data["journeyVector"], journeys=data["journeys"])
+
+def extract_time(date_time):
+    date_time_obj = datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
+    return date_time_obj.strftime("%H:%M")
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8000, debug=True)
