4a7abb7518432461b190d152ef55120491f40b9d
mostafahanafi <mostafahanafi2001@hotmail.com>
2024-02-19 16:09:20 +0000

    refactor; add Journey class

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests
-import json
-from datetime import datetime
+from Journey import Journey
 
 app = Flask(__name__)
 
@@ -16,47 +15,15 @@ def getroute():
 
     url = f"https://api.tfl.gov.uk/journey/journeyresults/{start}/to/{end}"
     response = requests.get(url)
-
     if response.status_code != 200:
         return render_template('error.html')
     
     data = response.json()
-
-    # Extract the time from the date time string
-    for journey in data["journeys"]:
-        journey["startDateTime"] = extract_time(journey["startDateTime"])
-        journey["arrivalDateTime"] = extract_time(journey["arrivalDateTime"])
-        journey["modes"] = []
-        for leg in journey["legs"]:
-            leg["departureTime"] = extract_time(leg["departureTime"])
-            leg["arrivalTime"] = extract_time(leg["arrivalTime"])
-        
-            # Add journey modes
-            journey["modes"].append(leg["mode"]["name"])
-
-    # return render_template('route.html', metadata=data["journeyVector"], journeys=data["journeys"])
-    route1 = extract_route_coordinates(data["journeys"][0])
-    return jsonify(route1)
-
-def extract_route_coordinates(journey):
-    leg_details = []
-    for leg in journey["legs"]:
-        lineString = json.loads(leg["path"]["lineString"])
-        mode = leg["mode"]["name"]
-        line = leg["routeOptions"][0]["name"]
-        leg_details.append({
-            "coordinates": lineString,
-            "mode": mode,
-            "line": line
-        })
-    return leg_details
-        
-
-
-def extract_time(date_time):
-    date_time_obj = datetime.strptime(date_time, '%Y-%m-%dT%H:%M:%S')
-    return date_time_obj.strftime("%H:%M")
-
+    journeys = [Journey(journey) for journey in data["journeys"]]
+    map_info = [journey.getMapData() for journey in journeys]
+    # for now, we will only return the first journey
+    # FIXME: We should return all the journeys
+    return jsonify(map_info[0])
 
 if __name__ == '__main__':
     app.run(port=8000, debug=True)
