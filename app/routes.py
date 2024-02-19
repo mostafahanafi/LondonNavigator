f63de9ded45695f2d01f5e838401d50a31d606e0
mostafahanafi <mostafahanafi2001@hotmail.com>
2024-02-19 22:42:40 +0000

    implement application factory pattern

---
+++ app/routes.py
@@ -0,0 +1,25 @@
+from flask import render_template, request, jsonify
+import requests
+from .models import Journey
+
+def init_routes(app):
+    @app.route('/', methods=['GET'])
+    def hello():
+        return render_template('index.html')
+
+    @app.route('/getroute', methods=['POST'])
+    def getroute():
+        start = request.form['start']
+        end = request.form['end']
+
+        url = f"https://api.tfl.gov.uk/journey/journeyresults/{start}/to/{end}"
+        response = requests.get(url)
+        if response.status_code != 200:
+            return render_template('error.html')
+        
+        data = response.json()
+        journeys = [Journey(journey) for journey in data["journeys"]]
+        map_info = [journey.getMapData() for journey in journeys]
+        # for now, we will only return the first journey
+        # FIXME: We should return all the journeys
+        return jsonify(map_info[0])
